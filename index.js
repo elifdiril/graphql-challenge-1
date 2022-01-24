eeb07b3f273ae51d922339a0661e56fb318c3af9
elifdiril <elifdiril6@gmail.com>
2022-01-24 11:52:46 +0300

    typedefs added

--- index.js
+++ index.js
@@ -3,14 +3,43 @@ const { ApolloServer, gql } = require('apollo-server');
 const { events, pariticipants, users, locations } = require('./data');
 
 const typeDefs = gql`
- 
+type Event {
+    id: ID!
+    title: String!
+    desc: String!
+    date: String!
+    from: String!
+    to: String!
+    location_id: Int!
+    user_id: Int!
+  }
+
+  type locations {
+    id: ID!
+    name: String!
+    desc: String!
+    lat: Float!
+    lng: Float!
+  }
+
+  type users {
+    id: ID!
+    username: String!
+    email: String!
+  }
+
+  type participants {
+    id: ID
+    user_id: Int!
+    event_id: Int!
+  }
 `;
 
 const resolvers = {
     Query: {
-      
+
     },
-  };
+};
 
 
 const server = new ApolloServer({
