96676ce5f93758d1c07babc5dfd57f8bdec53d11
elifdiril <elifdiril6@gmail.com>
2022-01-30 07:54:20 +0300

    data bugs resolved

--- server/src/graphql/resolvers/Query.js
+++ server/src/graphql/resolvers/Query.js
@@ -1,19 +1,19 @@
 export const Query = {
   events: (_, __, { db }) => db.events,
   event: (_, args, { db }) => {
-    const data = db.events.find((event) => event.id === args.id);
+    const data = db.events.find((event) => event.id == args.id);
     return data;
   },
 
   locations: (_, __, { db }) => db.locations,
   location: (_, args, { db }) => {
-    const data = db.locations.find((location) => location.id === args.id);
+    const data = db.locations.find((location) => location.id == args.id);
     return data;
   },
 
   users: (_, __, { db }) => db.users,
   user: (_, args, { db }) => {
-    const data = db.users.find((user) => user.id === args.id);
+    const data = db.users.find((user) => user.id == args.id);
     return data;
   },
 
