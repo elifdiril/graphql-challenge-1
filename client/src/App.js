d66b7c0ba90a59baa0d047a146a00cbc742bc36a
elifdiril <elifdiril6@gmail.com>
2022-02-05 22:12:42 +0300

    style changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,38 @@
 import EventList from "./EventList/EventList";
-import Event from './Event/Event';
+import Event from "./Event/Event";
+import NewEvent from "./NewEvent";
 import styles from "./styles.module.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Menu } from "antd";
+import { useState } from "react";
 
 function App() {
+  const [current, setCurrent] = useState("event");
+
+  const handleClick = (e) => {
+    setCurrent(e.key);
+  };
+
   return (
-    <Router>
+    <div className={styles.app}>
+      <Router>
+        <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
+          <Menu.Item key="events">
+            <Link to="/">Events</Link>
+          </Menu.Item>
+          <Menu.Item key="new-event">
+            <Link to="/new-event">New Event</Link>
+          </Menu.Item>
+        </Menu>
         <Switch>
-        <Route path="/event/:id">
+          <Route path="/event/:id">
             <div className={styles.app}>
               <Event />
+            </div>
+          </Route>
+          <Route path="/new-event">
+            <div className={styles.app}>
+              <NewEvent/>
             </div>
           </Route>
           <Route path="/">
@@ -18,7 +41,8 @@ function App() {
             </div>
           </Route>
         </Switch>
-    </Router>
+      </Router>
+    </div>
   );
 }
 
